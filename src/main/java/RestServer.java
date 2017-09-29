89389596ee94b5cbf54a69bb56fa5d8673c9848f
Amir Chohan <amir.x.chohan@gmail.com>
2017-09-30 00:13:09 +0100

    Hello World using Undertow server

--- src/main/java/RestServer.java
+++ src/main/java/RestServer.java
@@ -1,23 +1,21 @@
 import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
-/**
- * Created by Amir on 28/09/2017.
- */
 public class RestServer {
 
-    public static void main(String[] args) {
+    private static int PORT = 8080;
+    private static String HOSTNAME = "localhost";
 
+    private static void defaultHandler(HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send("Hello World");
+    }
+
+    public static void main(String[] args) {
         Undertow server = Undertow.builder()
-                .addHttpListener(8080, "localhost")
-                .setHandler(new HttpHandler() {
-                    public void handleRequest(HttpServerExchange exchange) throws Exception {
-                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                        exchange.getResponseSender().send("Hello World");
-                    }
-                }).build();
+                .addHttpListener(PORT, HOSTNAME, RestServer::defaultHandler)
+                .build();
         server.start();
     }
 }
